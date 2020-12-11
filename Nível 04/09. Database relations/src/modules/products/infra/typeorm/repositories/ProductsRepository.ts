d82727e40a0dae08e3765f3deca856f2dada1809
Matheus Foganholo <matheusfoganholo@hotmail.com>
2020-12-10 22:21:35 -0300

    Uploading challenge 09

---
+++ Nível 04/09. Database relations/src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,49 @@
+import { getRepository, Repository, In } from 'typeorm';
+
+import IProductsRepository from '@modules/products/repositories/IProductsRepository';
+import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
+import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
+import Product from '../entities/Product';
+
+interface IFindProducts {
+  id: string;
+}
+
+class ProductsRepository implements IProductsRepository {
+  private ormRepository: Repository<Product>;
+
+  constructor() {
+    this.ormRepository = getRepository(Product);
+  }
+
+  public async create({
+    name,
+    price,
+    quantity,
+  }: ICreateProductDTO): Promise<Product> {
+    const product = this.ormRepository.create({ name, price, quantity });
+    const createProduct = await this.ormRepository.save(product);
+    return createProduct;
+  }
+
+  public async findByName(name: string): Promise<Product | undefined> {
+    const findProduct = await this.ormRepository.findOne({ where: { name } });
+    return findProduct;
+  }
+
+  public async findAllById(products: IFindProducts[]): Promise<Product[]> {
+    const productsId = products.map(product => product.id);
+    const findAllProduct = await this.ormRepository.find({
+      where: { id: In(productsId) },
+    });
+    return findAllProduct;
+  }
+
+  public async updateQuantity(
+    products: IUpdateProductsQuantityDTO[],
+  ): Promise<Product[]> {
+    return this.ormRepository.save(products);
+  }
+}
+
+export default ProductsRepository;
---
+++ Nível 04/09. Database relations/src/shared/infra/typeorm/migrations/1607647674239-CreateOrdersProducts.ts
@@ -0,0 +1,74 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateOrdersProducts1601916096531
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders_products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'order_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'product_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'price',
+            type: 'numeric',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'integer',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'ForeignOrder',
+            columnNames: ['order_id'],
+            referencedTableName: 'orders',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+          {
+            name: 'ForeignProduct',
+            columnNames: ['product_id'],
+            referencedTableName: 'products',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders_products');
+  }
+}
